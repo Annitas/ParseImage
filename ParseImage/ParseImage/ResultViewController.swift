108a49317845ce6d6a7bfc8ad46eb963f319779e
Annitas <39757257+Annitas@users.noreply.github.com>
2024-02-12 23:51:07 +0600

    Complete parse images

--- ParseImage/ParseImage/ResultViewController.swift
+++ ParseImage/ParseImage/ResultViewController.swift
@@ -7,24 +7,54 @@
 
 import UIKit
 
-class ResultViewController: UIViewController {
-    var images: [String: String] = [:]
-    var imageUrl: String?
-    var imageSize: String?
-
-//    init() { //images: [String: String]
-//        super.init(nibName: nil, bundle: nil)
-////        self.images = images
-//        
-//    }
-    
-//    required init?(coder: NSCoder) {
-//        fatalError("init(coder:) has not been implemented")
-//    }
+final class ResultViewController: UIViewController {
+    var images = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        // Создаем вертикальный стек вид для размещения изображений и ссылок
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+        
+        // Добавляем изображения и ссылки на них в стек вид
+        for imageUrl in images {
+            var str = imageUrl
+            str.insert("s", at: imageUrl.index(imageUrl.startIndex, offsetBy: 4))
+            print(str)
+            if let url = URL(string: str) {
+                URLSession.shared.dataTask(with: url) { data, response, error in
+                    if let data = data, let image = UIImage(data: data) {
+                        DispatchQueue.main.async {
+                            let imageView = UIImageView(image: image)
+                            imageView.contentMode = .scaleAspectFit
+                            imageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+                            imageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+                            
+                            let label = UILabel()
+                            label.text = str
+                            label.textAlignment = .center
+                            
+                            stackView.addArrangedSubview(imageView)
+                            stackView.addArrangedSubview(label)
+                        }
+                    } else {
+                        print("Не удалось загрузить изображение по URL: \(str)")
+                    }
+                }.resume()
+            } else {
+                print("Некорректный URL: \(imageUrl)")
+            }
+        }
     }
 }
